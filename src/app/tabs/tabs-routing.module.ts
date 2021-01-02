8beb33e85adbe64452dddf2999bd40c679bd43ad
xArthurDev <arthurrguimaraes12@gmail.com>
2021-01-02 12:58:48 -0300

    Add all pages to tabs routing

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,26 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'a-details',
+        loadChildren: () => import('.././pages/a-details/a-details.module').then( m => m.ADetailsPageModule)
+      },
+      {
+        path: 'info-details',
+        loadChildren: () => import('.././pages/modal/info-details/info-details.module').then( m => m.InfoDetailsPageModule)
+      },
+      {
+        path: 'schedule-details',
+        loadChildren: () => import('.././pages/modal/schedule-details/schedule-details.module').then( m => m.ScheduleDetailsPageModule)
+      },
+      {
+        path: 'type-details',
+        loadChildren: () => import('../pages/modal/type-details/type-details.module').then( m => m.TypeDetailsPageModule)
+      },
+      {
+        path: 'rating-details',
+        loadChildren: () => import('../pages/modal/rating-details/rating-details.module').then( m => m.RatingDetailsPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
